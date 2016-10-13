79e6ef5e32ba15522dec26c38095e209772b345d
Daniel Mills <danielmillst@gmail.com>
2016-10-13 02:12:39 -0400

    Remove boom

--- src/main/java/org/cyberpwn/factionsplus/EndermanController.java
+++ src/main/java/org/cyberpwn/factionsplus/EndermanController.java
@@ -1,21 +1,12 @@
 package org.cyberpwn.factionsplus;
 
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Enderman;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.phantomapi.construct.Controllable;
 import org.phantomapi.construct.Controller;
-import org.phantomapi.lang.GList;
-import org.phantomapi.sync.TaskLater;
-import org.phantomapi.util.C;
-import org.phantomapi.util.M;
-import org.phantomapi.world.Cuboid;
 import de.dustplanet.util.SilkUtil;
 
 public class EndermanController extends Controller
@@ -41,34 +32,6 @@ public class EndermanController extends Controller
 		catch(Exception ex)
 		{
 			
-		}
-	}
-	
-	@SuppressWarnings("deprecation")
-	@EventHandler
-	public void on(ExplosionPrimeEvent e)
-	{
-		if(e.getEntityType().equals(EntityType.CREEPER))
-		{
-			Cuboid c = new Cuboid(e.getEntity().getLocation().add(3, 3, 3), e.getEntity().getLocation().add(-3, -3, -3));
-			new TaskLater(2)
-			{
-				@Override
-				public void run()
-				{
-					for(Block i : new GList<Block>(c.iterator()))
-					{
-						Location l = i.getLocation();
-						
-						if(l.getBlock().getType().equals(Material.MOB_SPAWNER) && M.r(0.45))
-						{
-							short k = u.getSpawnerEntityID(l.getBlock());
-							l.getBlock().setType(Material.AIR);
-							l.getWorld().dropItem(l, u.newSpawnerItem(k, C.YELLOW.toString() + u.getCreatureName(k) + C.WHITE.toString() + " Spawner"));
-						}
-					}
-				}
-			};
 		}
 	}
 	
